2b87d1467ff5774002bc6d29c793fbe88a945d7b
Stephen Burrows <stephen.r.burrows@gmail.com>
2011-09-17 15:39:11 -0700

    Removed vestigial admin.py file. Added HTML5 boilerplate and sass/compass integration.

--- ticketeer/urls.py
+++ ticketeer/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView
+
 from ticketeer.views import TicketDetailView, TicketListView
 
+
 urlpatterns = patterns( '',
-	url(r"^tickets/(?P<ticket_id>\d+)$",TicketDetailView.as_view(),name="ticket_detail"),
-	url(r"^tickets$",TicketListView.as_view(),name="ticket_list"),
+	url(r"^$", TemplateView.as_view(template_name="ticketeer/home.html"), name="ticketeer_home"),
+	url(r"^tickets/(?P<ticket_id>\d+)$", TicketDetailView.as_view(), name="ticketeer_ticket_detail"),
+	url(r"^tickets$", TicketListView.as_view(), name="ticketeer_ticket_list"),
 )
